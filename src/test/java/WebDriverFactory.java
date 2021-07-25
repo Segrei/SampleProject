8afa6f250628e04e29d802b328ae1df1ba457d6f
Sergei <kserg-068@yandex.ru>
2021-07-25 06:00:15 +0300

    ДЗ2

---
+++ src/test/java/WebDriverFactory.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class WebDriverFactory {
+  private static Logger logger = LogManager.getLogger(WebDriverFactory.class);
+
+  public static WebDriver getDriver(String browserName) {
+    switch (browserName) {
+      case "chrome":
+        WebDriverManager.chromedriver().setup();
+        logger.info("Драйвер для браузера Google Chrome");
+        return new ChromeDriver();
+      case "firefox":
+        WebDriverManager.firefoxdriver().setup();
+        logger.info("Драйвер для браузера Mozilla Firefox");
+        return new FirefoxDriver();
+      default:
+        throw new RuntimeException("Incorrect browser name");
+    }
+  }
+}
